90eb1eaaa137a8c15b474a6b34fd714165099ec7
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-09-14 17:08:08 +0900

    fix mixinの動作確認用ファイルを追加

---
+++ src/components/Sample.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>動作確認用ページ</p>
+    <CreateList />
+    <div>{{lists[0].name}}</div>
+  </div>
+</template>
+
+<script>
+import CreateList from "./shared/CreateList.vue";
+
+export default {
+  components: {
+    CreateList,
+  },
+
+  data() {
+    return {
+      lists: {},
+    };
+  },
+
+  created() {
+    this.lists = this.getOwnedListsFromUserId("4oFo1QKy3X8wGwuGx98h");
+    console.debug("fuga:",this.lists);
+  },
+};
+</script>
+
+<style>
+</style>
