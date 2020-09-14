7e708dd4e5dd0786b424d5d0a923aa919bf921c7
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-09-14 15:18:29 +0900

    update 最低限のリストを新規作成できるようになった

---
+++ src/components/shared/CreateList.vue
@@ -0,0 +1,43 @@
+<template>
+  <button @click="createList()">リストを新規作成</button>
+</template>
+
+<script>
+import firebase from "firebase";
+import "firebase/firestore";
+
+export default {
+  name:"CreateList",
+
+  data() {
+    return {
+      listName: "sampleList",
+      db:null
+    };
+  },
+
+  created(){
+    this.db=firebase.firestore()
+  },
+
+  methods: {
+    createList() {
+      this.db.collection("lists")
+        .add({
+          name: this.listName,
+        })
+        .then(() => {
+          alert(this.listName + "を新規作成しました。");
+        })
+        .catch(() => {
+          alert(this.listName + "を作成するときにエラーが発生しました。");
+        });
+    },
+  },
+
+
+};
+</script>
+
+<style>
+</style>
