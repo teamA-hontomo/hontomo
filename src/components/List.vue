52bee8936c7328dc06cae9ad5e4ec1e0f112f53e
青谷和真 <aotani@aoyawashinnoMacBook-Pro.local>
2020-09-14 16:21:34 +0900

    リスト詳細ページの部分的な作成

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,12 +1,16 @@
 <template v-if="true">
   <div id='list'>
-    <TitleBox v-bind:title='id + "のリスト"'>
+    <TitleBox v-bind:title='name'>
+      <font-awesome-icon v-on:click='add' v-if='!fav' icon='star' slot='button' class='fa-lg' />
+      <font-awesome-icon v-on:click='remove' v-if='fav' icon='star' slot='button' class='fa-lg star_yellow' />
+      <span slot='button'>{{rating}}</span>
       <button v-on:click='onClick' v-if='open' slot='button' class='btn btn-success'>公開</button>
       <button v-on:click='onClick' v-if='!open' slot='button' class='btn btn-danger'>非公開</button>
     </TitleBox>
     <ContentsBox>
       <span>コンテンツ</span>
     </ContentsBox>
+
   </div>
 </template>
 <script>
@@ -17,6 +21,13 @@ export default {
     return {
       id: this.$route.params.id,
       open: true,
+      rating: 5,
+      list_open: true,
+      name: "hoge",
+      fav:false,
+      author:false,
+      date:"dd",
+      //nakami:;
     };
   },
   components: {
@@ -27,6 +38,19 @@ export default {
     onClick: function () {
       this.open = !this.open;
     },
+    add: function () {
+      this.rating++;
+      this.fav = !this.fav;
+    },
+    remove: function () {
+      this.rating--;
+      this.fav = !this.fav;
+    },
+
   },
 };
 </script>
+
+<style>
+.star_yellow{color:#ffff00;}
+</style>
