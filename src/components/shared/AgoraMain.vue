a1d756adcacd19e295adb013f96441d684be3b81
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2020-09-16 12:19:51 +0900

    update サンプルページの右端に表示するようにした

--- src/components/shared/AgoraMain.vue
+++ src/components/shared/AgoraMain.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
+  <div class="agora">
     <div v-for="message in messages" :key="message.id">
       <Message :message="message" />
     </div>
+    
   </div>
 </template>
 
@@ -20,11 +21,33 @@ export default {
           text: "hogehgoe",
           userId: "4oFo1QKy3X8wGwuGx98h",
         },
+        {
+          created: "2020/4/1",
+          id: "qYYDFYQIqCNxawdfFlbj",
+          text: "hogehgoe",
+          userId: "4oFo1QKy3X8wGwuGx98h",
+        },
+        {
+          created: "2020/4/1",
+          id: "qYYDFYQIqCNxawdfFlbk",
+          text: "hogehgoe",
+          userId: "4oFo1QKy3X8wGwuGx98h",
+        },
+        {
+          created: "2020/4/1",
+          id: "qYYDFYQIqCNxawdfFlbl",
+          text: "hogehgoe",
+          userId: "4oFo1QKy3X8wGwuGx98h",
+        },
       ],
     };
   },
 };
 </script>
 
-<style>
+<style scoped>
+.agora {
+  height: 100vh;
+  overflow-y: scroll;
+}
 </style>
