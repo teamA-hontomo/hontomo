8fce35fd4fa34fb407e744b23e6fee89f83ec3c2
gyuta <kuroshiba0408@gmail.com>
2020-09-16 16:17:49 +0900

    ビューワーのレイアウト修正

--- src/components/View.vue
+++ src/components/View.vue
@@ -1,16 +1,44 @@
 <template>
-  <div class='row'>
+  <div class="row">
     <div class="col-8">
-      <div id='view-wrap'>
-        <div id='go' class='btn' @click='go'>進む</div>
-        <div id='view'></div>
-        <div id='back' class='btn' @click='back'>戻る</div>
+      <div id="view-wrap">
+        <div id="go" class="btn" @click="go">
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 16 16"
+            class="bi bi-chevron-left"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+            />
+          </svg>
+        </div>
+        <div id="view"></div>
+        <div id="back" class="btn" @click="back">
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 16 16"
+            class="bi bi-chevron-right"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+            />
+          </svg>
+        </div>
       </div>
     </div>
     <div class="col-4">
       <AgoraMain></AgoraMain>
     </div>
-    <FrameSaveModal :frame_src='selected_frame' ref='modal'></FrameSaveModal>
+    <FrameSaveModal :frame_src="selected_frame" ref="modal"></FrameSaveModal>
   </div>
 </template>
 
@@ -25,7 +53,7 @@ export default {
   data() {
     return {
       viewer: {},
-      selected_frame: "",
+      selected_frame: ""
     };
   },
   mounted() {
@@ -33,8 +61,8 @@ export default {
       xml_src: "/static/books/test.xml",
       listClick: this.listClick,
       agoraClick: this.agoraClick,
-      canvas_width: 400,
-      canvas_height: 600,
+      canvas_width: 500,
+      canvas_height: 750
     });
 
     this.viewer.show();
@@ -54,8 +82,8 @@ export default {
     },
     agoraClick() {
       alert("agoraClick!!!");
-    },
-  },
+    }
+  }
 };
 </script>
 
